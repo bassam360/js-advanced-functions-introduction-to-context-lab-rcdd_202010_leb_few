e099afc6f3d9c1736d589023ab545e4dbda8530c
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-26 15:41:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,26 +7,18 @@ let createEmployeeRecord = function(row){
         payPerHour: row[3],
         timeInEvents: [],
         timeOutEvents: []
-    }
-}
+    }}
 let createEmployeeRecords = function(employeeRowData) {
     return employeeRowData.map(function(row){
-        return createEmployeeRecord(row)
-    })
-}
-
+        return createEmployeeRecord(row)  })}
 let createTimeInEvent = function(employee, dateStamp){
     let [date, hour] = dateStamp.split(' ')
-
     employee.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
-        date,
-    })
-
-    return employee
+        date})
+return employee
 }
-
 let createTimeOutEvent = function(employee, dateStamp){
     let [date, hour] = dateStamp.split(' ')
     employee.timeOutEvents.push({
