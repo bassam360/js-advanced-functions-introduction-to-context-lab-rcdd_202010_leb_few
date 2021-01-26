07bc7be16225a28bca02e9a717c9546f5216f4f4
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-26 15:40:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,37 +45,27 @@ let hoursWorkedOnDate = function(employee, soughtDate){
     })
 
     let outEvent = employee.timeOutEvents.find(function(e){
-        return e.date === soughtDate
+     return e.date === soughtDate
     })
-
-    return (outEvent.hour - inEvent.hour) / 100
+return (outEvent.hour - inEvent.hour) / 100
 }
-
 let wagesEarnedOnDate = function(employee, dateSought){
     let rawWage = hoursWorkedOnDate(employee, dateSought)
-        * employee.payPerHour
-    return parseFloat(rawWage.toString())
-}
-
+     * employee.payPerHour
+    return parseFloat(rawWage.toString())}
 let allWagesFor = function(employee){
     let eligibleDates = employee.timeInEvents.map(function(e){
-        return e.date
+  return e.date
     })
-
     let payable = eligibleDates.reduce(function(memo, d){
         return memo + wagesEarnedOnDate(employee, d)
     }, 0)
-
-    return payable
-}
+    return payable}
 let findEmployeeByFirstName = function(srcArray, firstName) {
   return srcArray.find(function(rec){
     return rec.firstName === firstName
-  })
-}
-
+  })}
 let calculatePayroll = function(arrayOfEmployeeRecords){
     return arrayOfEmployeeRecords.reduce(function(memo, rec){
-        return memo + allWagesFor(rec)
-    }, 0)
+        return memo + allWagesFor(rec) }, 0)
 }
